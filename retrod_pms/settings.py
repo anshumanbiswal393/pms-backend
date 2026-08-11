9439a2258edc06a3f12c26c9269a2ba53bfa7262
yagyandattamurmu <yagyandatta.murmu@cozentus.com>
2026-08-11 21:39:09 +0530

    Add Docker deployment setup
    - Dockerfile with gunicorn entrypoint (migrate + collectstatic on boot)
    - docker-compose.yml wiring postgres, redis, backend, and sibling frontend
    - whitenoise for serving static files (admin/swagger/DRF browsable API)
    - .env.example documenting required runtime and build-time vars

--- retrod_pms/settings.py
+++ retrod_pms/settings.py
@@ -71,6 +71,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -146,6 +147,14 @@ USE_TZ = True
 # Static files
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STORAGES = {
+    'default': {
+        'BACKEND': 'django.core.files.storage.FileSystemStorage',
+    },
+    'staticfiles': {
+        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+    },
+}
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
