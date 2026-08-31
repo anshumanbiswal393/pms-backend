743833d04735ce920a73acc892cc0f709c0a43b3
Samprita Patra <Sampritapatra123@gmail.com>
2026-08-31 11:18:59 +0530

    fix(cors): add https://app.dev.retrod.in to CORS_ALLOWED_ORIGINS

--- retrod_pms/settings.py
+++ retrod_pms/settings.py
@@ -181,7 +181,8 @@ CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS', default=[
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "http://144.31.146.64:3000"
+    "http://144.31.146.64:3000",
+    "https://app.dev.retrod.in"
 ])
 
 CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=[])
